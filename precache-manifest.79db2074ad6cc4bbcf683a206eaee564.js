4725566fab7f150d7f0f9920d42aab0f74e3a152
Terra Andoh Baffoe <terra.andoh@gmail.com>
2019-11-02 08:19:33 +0000

    Updates

---
+++ precache-manifest.79db2074ad6cc4bbcf683a206eaee564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55341330afbd9d3f46a6ba5ce08882f3",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "89dedb377862da50b558",
+    "url": "/my-page/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "20f7cef27237b49a00d0",
+    "url": "/my-page/static/js/2.3ebf7471.chunk.js"
+  },
+  {
+    "revision": "89dedb377862da50b558",
+    "url": "/my-page/static/js/main.c61e8bc5.chunk.js"
+  },
+  {
+    "revision": "60af47ad517adfbbb877",
+    "url": "/my-page/static/js/runtime-main.bccbd599.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-page/static/media/logo.25bf045c.svg"
+  }
+]);
